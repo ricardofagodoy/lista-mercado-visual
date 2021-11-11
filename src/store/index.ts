540abf437d8243a7845d9ee474db26879b1f5198
Ricardo Godoy <ricardofagodoy@gmail.com>
2021-11-11 11:53:28 -0300

    Readme

--- src/store/index.ts
+++ src/store/index.ts
@@ -11,23 +11,7 @@ export default new Vuex.Store({
         title: "Coca-cola 2l",
         image:
           "https://media.istockphoto.com/photos/cola-glass-with-ice-cubes-picture-id530428650?k=20&m=530428650&s=612x612&w=0&h=I5HJCAovm-Ok3hFdmmwh3kurV0xsdIar4fAQ8FxkexI=",
-      },
-      {
-        id: 2,
-        title: "Coca-cola 2l",
-      },
-      {
-        id: 3,
-        title: "Coca-cola 2l aaaaqwqwdefaaaaqwqwdefaaaaq wqwdefaaaa qwqwdefaaaaqwqwdef",
-        image:
-          "https://media.istockphoto.com/photos/cola-glass-with-ice-cubes-picture-id530428650?k=20&m=530428650&s=612x612&w=0&h=I5HJCAovm-Ok3hFdmmwh3kurV0xsdIar4fAQ8FxkexI=",
-      },
-      {
-        id: 4,
-        title: "Coca-cola 2l",
-        image:
-          "https://media.istockphoto.com/photos/cola-glass-with-ice-cubes-picture-id530428650?k=20&m=530428650&s=612x612&w=0&h=I5HJCAovm-Ok3hFdmmwh3kurV0xsdIar4fAQ8FxkexI=",
-      },
+      }
     ],
   },
   getters: {
